2dabdeb0e4f593d8b2825ce331a4f515225fe3b8
andrewbav <andrewbav@mail.ru>
2015-11-18 15:08:10 +0300

    Revert "132"
    This reverts commit 8c8d90b72490b624b97e98a066c2930c1c5076db.

--- PayrollTest_BAV/PayrollTest_BAV.cs
+++ PayrollTest_BAV/PayrollTest_BAV.cs
@@ -10,14 +10,13 @@ namespace PayrollTest_BAV
         [TestMethod]
         public void TestEmployee()
         {
-            int empId = 0;
+            int empId = 1;
             Employee e = new Employee(empId, "Bob", "Home");
             Assert.AreEqual("Bob",e.Name);
             Assert.AreEqual("Home", e.Address);
             Assert.AreEqual(empId, e.EmpId);
         }
 
-         [TestMethod]
         public void TestAddSalariedEmployee()
         {
             int empId = 1;
@@ -35,10 +34,9 @@ namespace PayrollTest_BAV
             Assert.IsTrue(pm is HoldMethod);
         }
 
-        [TestMethod]
         public void TestAddHourlyEmployee()
         {
-            int empId = 2;
+            int empId = 1;
             AddHourlyEmployee t = new AddHourlyEmployee(empId, "Bob", "Home", 132.00);
             t.Execute();
             Employee e = PayrollDatabase.GetEmployee(empId);
@@ -52,11 +50,10 @@ namespace PayrollTest_BAV
             PaymentMethod pm = e.Method;
             Assert.IsTrue(pm is HoldMethod);
         }
-        
-        [TestMethod]
+
         public void TestAddCommissionedEmployee()
         {
-            int empId = 3;
+            int empId = 1;
             AddComissionedEmployee t = new AddComissionedEmployee(empId, "Bob", "Home", 100.00, 1000.00);
             t.Execute();
             Employee e = PayrollDatabase.GetEmployee(empId);
@@ -67,23 +64,9 @@ namespace PayrollTest_BAV
             Assert.AreEqual(1000.00, sc.Salary, .001);
             Assert.AreEqual(100.00, sc.CRate, .001);
             PaymentSchedule ps = e.Schedule;
-            Assert.IsTrue(ps is BiweeklySchedule);
+            Assert.IsTrue(ps is HourlySchedule);
             PaymentMethod pm = e.Method;
             Assert.IsTrue(pm is HoldMethod);
-        }
-
-        [TestMethod]
-        public void DeleteEmployee()
-        {
-            int empid = 4;
-            AddSalariedEmployee t = new AddSalariedEmployee(empid,"Bill", "home",2000);
-            t.Execute();
-            Employee e = PayrollDatabase.GetEmployee(empid);
-            Assert.IsNotNull(e);
-            DeleteEmployeeTransaction dt = new DeleteEmployeeTransaction(empid);
-            dt.Execute();
-            e = PayrollDatabase.GetEmployee(empid);
-            Assert.IsNull(e);
         }
     }
 }
